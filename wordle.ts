17bb44af64a31bd11957a6a0020830852f7479c5
Landon Schropp <schroppl@gmail.com>
2022-01-31 15:52:42 -0600

    Ensure misplaced letters are not in the wrong spot

--- wordle.ts
+++ wordle.ts
@@ -5,6 +5,7 @@ import { sample } from "lodash-es";
  */
 export function validateResponse(response: string) {
   if (!/^\w{5}$/.test(response)) {
+    // eslint-disable-next-line no-console
     console.error(`\nThe response '${ response }' is not valid!`);
     process.exit(1);
   }
@@ -33,6 +34,14 @@ function doesNotContainRejectedLetters(word: string, attempt: string, response: 
 }
 
 /**
+ * Returns true if the word does not contain any of the misplaced letters where they're not an exact
+ * match.
+ */
+function doesNotHaveMisplacedLettersInWrongSpot(word: string, attempt: string, response: string) {
+  return !new RegExp(response.replaceAll(/[A-Z_]/g, ".")).test(word);
+}
+
+/**
  * Returns true if the word contains all of the letters that were present in the response but were
  * not in the correct location.
  */
@@ -48,6 +57,7 @@ function conatainsMisplacedLetters(word: string, attempt: string, response: stri
 function isMatch(word: string, attempt: string, response: string) {
   return matchesExactLetters(word, response)
     && doesNotContainRejectedLetters(word, attempt, response)
+    && doesNotHaveMisplacedLettersInWrongSpot(word, attempt, response)
     && conatainsMisplacedLetters(word, attempt, response);
 }
 
