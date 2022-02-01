18cf0a457412301a439e6d1200302908c9cf1696
Landon Schropp <schroppl@gmail.com>
2022-02-01 13:09:28 -0600

    Move files under source directory

--- source/dictionary.ts
+++ source/dictionary.ts
@@ -4,5 +4,5 @@ import { fileURLToPath } from "url";
 
 const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
 
-export const DICTIONARY = readFileSync(path.join(DIRNAME, "data/dictionary.txt"), "utf-8")
+export const DICTIONARY = readFileSync(path.join(DIRNAME, "../data/dictionary.txt"), "utf-8")
   .split("\n");
