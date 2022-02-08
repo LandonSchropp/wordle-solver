cbe066515a09102652506d7bfb49e8828772e0d4
Landon Schropp <schroppl@gmail.com>
2022-02-07 19:42:57 -0600

    Fix environment with dictionary file

--- source/dictionary.ts
+++ source/dictionary.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 
-export const DICTIONARY = readFileSync(
-  "/Users/landon/Development/personal/wordle-solver/data/dictionary.txt",
-  "utf-8"
-).split("\n");
+// HACK: Unfortunately, the testing environment and runtime environment don't share the same set of
+// global variables used to determine the current directory name. This is a quick workaround to get
+// the project moving forward. Ideally, these environments would be brought into alignment.
+export const DICTIONARY = readFileSync("data/dictionary.txt", "utf-8").split("\n");
